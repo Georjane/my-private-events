b326086f19423d3e7a2233320778e9db8384cc32
Georjane <pzgeorjane@gmail.com>
2020-09-07 12:59:58 +0100

    Displayed Past and Upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :event_creator, class_name: "User"
+  
   has_many :attended_events, foreign_key: "attended_event_id", class_name: "Attend"
   has_many :event_attendees, through: :attended_events
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events, foreign_key: "event_creator_id"
+  
   has_many :event_attendees, foreign_key: "event_attendee_id", class_name: "Attend"
   has_many :attended_events, through: :event_attendees
 end
